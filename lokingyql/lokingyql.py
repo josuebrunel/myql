5d498311ae572a91cc02f5423abac0e1c11c5eb5
josuebrunel <josuebrunel@gmail.com>
2014-08-16 14:15:54 +0200

    fix #8 yql.use(<new_url>) OK

--- lokingyql/lokingyql.py
+++ lokingyql/lokingyql.py
@@ -1,14 +1,19 @@
 import requests
 
 class LokingYQL(object):
-
-  def __init__(self, table=None, format='json'):
-    self.url = 'https://query.yahooapis.com/v1/public/yql'	  
+  '''Yet another Python Yahoo! Query Language Wrapper
+  '''
+  default_url = 'https://query.yahooapis.com/v1/public/yql'	  
+  
+  def __init__(self, table=None, url=default_url, format='json'):
+    self.url = url
     self.table = table
     self.format = format
 
   def __repr__(self):
-    pass
+    '''Returns information on the current instance
+    '''
+    return "<url>: '{0}' - <table>: '{1}' -  <format> : '{2}' ".format(self.url, self.table, self.format)
 
   def payloadBuilder(self, query, format='json'):
     '''Build the payload'''
@@ -32,7 +37,7 @@ class LokingYQL(object):
     #if pretty :
     #  response = self.buildResponse(response)
 
-    return response.json()
+    return response
 
   def executeQuery(self, payload):
     '''Execute the query and returns and response'''
@@ -41,7 +46,8 @@ class LokingYQL(object):
     return response
 
   def buildResponse(self, response):
-    
+    '''Try to return a pretty formatted response object
+    '''
     try:
       r = response.json()
       result = r['query']['results']['table']
@@ -63,8 +69,16 @@ class LokingYQL(object):
   #
   #####################################################
 
+  def use(self, url):
+    '''Changes the data provider
+    '''
+    self.url = url
+    return self.url
+
   def select(self, table=None):
-    '''This method simulate a select on a table'''
+    '''This method simulate a select on a table
+       >>> yql.select('table')
+    '''
     try:
       self.current_table = table
     except Exception, e:
@@ -72,11 +86,11 @@ class LokingYQL(object):
 
     return self
 
-  def where(self, *args, **kwargs):
+  def where(self, *args):
      ''' This method simulates a where condition. Use as follow:
-         yql.select('mytable').where(dict)
+         >>>yql.select('mytable').where([('name', '=', 'alain'), ('location', '!=', 'paris')])
      '''
-     pass
+     return None
 
   def showTables(self, format='json'):
     '''Return list of all avaible tables'''
