66f368a4c258a9764f304e1de32d34544b714e68
josuebrunel <josuebrunel@gmail.com>
2014-08-30 16:13:18 +0200

    #12 authorize_url method OK

--- lokingyql/yahooauth.py
+++ lokingyql/yahooauth.py
@@ -1,3 +1,4 @@
+import webbrowser
 from rauth import OAuth1Service
 
 class YahooOAuth(object):
@@ -21,11 +22,35 @@ class YahooOAuth(object):
       base_url = 'https://query.yahooapis.com/v1/public/yql'
     )
 
+    self.oauth_params {
+      # 'oauth_consumer_key' : 'dj0yJmk9aVRSd3ZabElmTzJNJmQ9WVdrOWEyNW1VRmRGTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1hMg--',
+      'oauth_nonce' : uuid4().hex,
+      'oauth_timestamp': time.time(),
+      'oauth_signature_method' : 'plaintext',
+      'oauth_signature' : '98407bfba41094aacd571e971fe82c9c7c1cfe60%26',
+      'oauth_version' : 1.0,
+      'oauth_callback' : 'oob'
+    }
 
-  def request_token(self,):
-    '''Requests access_token
+
+  def get_request_token(self,):
+    '''Requests access_token and access secrets
     '''
 
-    request_token, request_token_secret = self.oauth.get_request_token(params= {"oauth_callback": 'oob'})
+    self.request_token, self.request_token_secret = self.oauth.get_request_token(params= self.oauth_params)
 
-    return request_token, request_token_secret
+    return self.request_token, self.request_token_secret
+
+  def authorize_url(self,):
+    '''Redirects to the authorization url
+    '''
+
+    url = self.oauth.get_authorize_url(self.request_token)
+
+    webbrowser.open(str(url))
+
+    verifier = raw_input('Please input the verifier : ')
+
+    return verifier
+
+  
