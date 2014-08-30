1f7da787403b1eaf92045f055201d0625c5dcadd
josuebrunel <josuebrunel@gmail.com>
2014-08-30 21:09:25 +0200

    updates #12, restructure the project

--- lokingyql/yahooauth.py
+++ lokingyql/yahooauth.py
@@ -7,13 +7,14 @@ class YahooOAuth(object):
   '''OAuth for yahoo api
   '''
 
-  def __init__(self, consumer_key= None, consumer_secret= None, base_url= None):
+  default_base_url = 'https://query.yahooapis.com/v1/public/yql'
+
+  def __init__(self, consumer_key= None, consumer_secret= None, base_url= default_base_url):
     '''
     '''
     self.consumer_key = consumer_key
     self.consumer_secret = consumer_secret
     self.base_url = base_url
-
     self.service = OAuth1Service(
       consumer_key = consumer_key,
       consumer_secret = consumer_secret,
@@ -21,15 +22,10 @@ class YahooOAuth(object):
       request_token_url = "https://api.login.yahoo.com/oauth/v2/get_request_token",
       access_token_url = "https://api.login.yahoo.com/oauth/v2/get_token",
       authorize_url = "https://api.login.yahoo.com/oauth/v2/request_auth",
-      base_url = 'https://query.yahooapis.com/v1/public/yql'
+      base_url = base_url
     )
 
     self.params = {
-      # 'oauth_consumer_key' : 'dj0yJmk9aVRSd3ZabElmTzJNJmQ9WVdrOWEyNW1VRmRGTnpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1hMg--',
-      # 'oauth_nonce' : uuid4().hex,
-      # 'oauth_timestamp': time.time(),
-      #'oauth_signature_method' : 'plaintext',
-      #'oauth_signature' : '98407bfba41094aacd571e971fe82c9c7c1cfe60%26',
       'oauth_version' : 1.0,
       'oauth_callback' : 'oob'
     }
