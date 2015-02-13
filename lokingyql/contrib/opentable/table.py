b60f6c55d1a79052a300d0c90b0299537495a78a
josuebrunel <josuebrunel@gmail.com>
2015-02-13 06:35:47 -0600

    table xml data creation

--- lokingyql/contrib/opentable/table.py
+++ lokingyql/contrib/opentable/table.py
@@ -1,21 +1,44 @@
-from xml.etree import cElementTree
+from xml.etree import cElementTree as xtree
 
-class Table(object):
+class YqlTable(object):
     """Class representating a YQL Table
     """
 
-    def __init__(self, name, author, apiKey, documentationURL, description, sampleQuery):
+    _TAB_ATTR = {'xmlns':'', 'securityLevel':'any', 'https':'false'}
+
+    def __init__(self, name, author, apiKeyURL, documentationURL, sampleQuery, description=None, table_attr=None):
         """Initialize the class
         """
         self.name = name
         self.author = author
-        self.apiKey = apiKey
+        self.apiKey = apiKeyURL
         self.documentationURL = documentationURL
         self.description = description
         self.sampleQuery = sampleQuery
+        self.table_attr = table_attr
+
+    def _create_table_xml_file(self, data):
+        """Creates a xml file of the table
+        """
+        import pdb
+        pdb.set_trace()
+        content = xtree.tostring(data, pretty_print= True)
+
+        with open(self.name+".xml", 'w') as f:
+            f.write(content)
 
 
-    def create_file(self, xml=True, db_table=True):
+    def save(self, xml=True, db_table=True):
         """Create a table 
         """
-        pass
+        # <table> tag object
+        t_table =  xtree.Element('table')
+        # <table xmlns='' securityLevel='' htpps=''>
+        if not self.table_attr :
+            self.table_attr = self._TAB_ATTR
+        for attr in self.table_attr.items() :
+            t_table.set(*attr)
+        
+        self._create_table_xml_file(t_table)
+    
+        
