e8f4049bb4ae1bdd4a14565b28817224c95de121
josuebrunel <josuebrunel@gmail.com>
2015-02-13 10:17:36 -0600

    testing module

---
+++ lokingyql/contrib/opentable/test.py
@@ -0,0 +1,42 @@
+import pdb
+from xml.etree import cElementTree as xtree
+from binder import Binder, BinderKey
+from yqltable import YqlTable
+
+import readline, rlcompleter
+readline.parse_and_bind('tab: complete')
+
+def dump(data):
+    print xtree.tostring(data.etree)
+
+
+stuff = {
+    'name': 'mytest',
+    'author': 'josuebrunel',
+    'apiKeyURL': 'http://josuebrunel.org/api',
+    'documentationURL': 'http://josuebrunel.org/doc.html',
+    'sampleQuery': 'SELECT * FROM mytable',
+}
+
+select = {
+    'name': 'select',
+    'itemPath': 'products.product',
+    'produces': 'xml'
+}
+
+b_select = Binder(**select)
+
+key = {
+    'id': 'artist',
+    'type': 'xs:string',
+    'paramType': 'path'
+}
+
+b_key = BinderKey(**key)
+
+b_select.addInput(b_key)
+
+pdb.set_trace()
+
+#table = YqlTable(**stuff)
+#table.save()
