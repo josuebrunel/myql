e3c974aefa0ab7604c90c429b2c222d00d2f6a14
josuebrunel <josuebrunel@gmail.com>
2015-03-03 06:25:43 -0600

    run_test script added

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+##################################################
+#
+#   Author          : yosuke
+#   Filename        : run_tests.sh
+#   Description     :
+#   Creation Date   : 03-03-2015
+#   Last Modified   : Tue 03 Mar 2015 06:23:05 AM CST
+#
+##################################################
+
+
+python -m unittest tests.LokingYqlTest
