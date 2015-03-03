502e9e00a82cbbba96203f5a42914c0642a99692
josuebrunel <josuebrunel@gmail.com>
2015-03-03 06:26:14 -0600

    yahooauth in is own package now

---
+++ lokingyql/contrib/yahooauth/__init__.py
@@ -0,0 +1,11 @@
+##################################################
+#
+#   Author          : yosuke
+#   Filename        : __init__.py
+#   Description     :
+#   Creation Date   : 03-03-2015
+#   Last Modified   : Tue 03 Mar 2015 06:24:16 AM CST
+#
+##################################################
+
+from yahooauth import YahooOAuth
