34c2f69e5b5e2fd59bfd2d53f66b4eb48c347527
josuebrunel <josuebrunel@gmail.com>
2015-03-03 00:19:29 -0600

    updating error classes

--- lokingyql/errors.py
+++ lokingyql/errors.py
@@ -1,7 +1,9 @@
 class NoTableSelectedError(Exception):
   '''Error raised when no table has been selected
   '''
-  def __init__(self, msg):
+  def __init__(self, msg=None):
+    if not msg:
+      msg = 'No table selected'
     self.msg = msg
 
   def __str__(self):
@@ -11,7 +13,10 @@ class NoConfigFileError(Exception):
   '''Error raised when the config file passed
   doesn't exist 
   '''
-  def __init__(self, msg):
+  def __init__(self, msg=None):
+    if not msg:
+      msg = 'Config file is invalid or doesn\'t exist'
+
     self.msg = msg
 
   def __str__(self,):
@@ -21,7 +26,10 @@ class NoConfigFileError(Exception):
 class NoConfigParameter(Exception):
   '''Error raised when config parameters don't exist
   '''
-  def __init__(self, msg):
+  def __init__(self, msg=None):
+    if not msg:
+      msg = 'Invalid config parameters'
+
     self.msg = msg
 
   def __str__(self,):
