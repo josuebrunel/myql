711c88f84f86370b8733ccfc83dd084d22b350fb
josuebrunel <josuebrunel@gmail.com>
2015-03-10 09:35:22 -0500

    cleaning the cat house

--- lokingyql/contrib/opentable/jscode.js
+++
@@ -1,5 +0,0 @@
- // Include the flickr signing library
- y.include("http:blog.pipes.yahoo.net/wp-content/uploads/flickr.js");
- // GET the flickr result using a signed url
- var fs = new flickrSigner(api_key,secret);
- response.object = y.rest(fs.createUrl({method:method, format:""})).get().response();
