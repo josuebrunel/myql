790239d3da9e5505d929b2d19c7b134185d1417d
josuebrunel <josuebrunel@gmail.com>
2015-03-10 09:35:02 -0500

    restructure data stuff

--- lokingyql/contrib/opentable/tests.py
+++ lokingyql/contrib/opentable/tests.py
@@ -38,7 +38,7 @@ class TestYqlTable(unittest.TestCase):
         }
 
         self.key = BinderKey(**self.key_desc)
-        self.key2 = BinderKey(id='song', type='xs:string', paramType='paht', required='true')
+        self.key2 = BinderKey(id='song', type='xs:string', paramType='path', required='true')
 
     def xml_pretty_print(self, data):
         """Pretty print xml data
@@ -54,15 +54,15 @@ class TestYqlTable(unittest.TestCase):
     def test_remove_binder(self,):
         self.binder.addInput(self.key)
         self.binder_insert.addInput(self.key)
-        self.binder.addFunction('', from_file='jscode.js')
+        self.binder.addFunction('', from_file='tests_data/jscode.js')
         self.binder_insert.addFunction("console.log('hello this is an insert function'); ")
         self.table.addBinder(self.binder)
         self.table.addBinder(self.binder_insert)
-        self.table.save(name='before')
-        self.assertEqual(os.path.isfile('before.xml'),True)
+        self.table.save(name='before', path='tests_data')
+        self.assertEqual(os.path.isfile('tests_data/before.xml'),True)
         self.table.removeBinder('select')
-        self.table.save(name='after')
-        self.assertEqual(os.path.isfile('after.xml'),True)
+        self.table.save(name='after', path='tests_data')
+        self.assertEqual(os.path.isfile('tests_data/after.xml'),True)
 
     def test_add_input_to_binder(self,):
         self.assertEqual(self.binder.addInput(self.key),True)
@@ -76,7 +76,7 @@ class TestYqlTable(unittest.TestCase):
         print self.xml_pretty_print(self.binder.etree)
 
     def test_add_function_from_file(self,):
-        self.assertEqual(self.binder.addFunction('', from_file='jscode.js'),True)
+        self.assertEqual(self.binder.addFunction('', from_file='tests_data/jscode.js'),True)
         print self.xml_pretty_print(self.binder.etree)
 
     def test_save_file(self,):
@@ -84,45 +84,45 @@ class TestYqlTable(unittest.TestCase):
         self.assertEquals(os.path.isfile('mytest.xml'),True) 
 
     def test_save_with_another_name(self):
-        name = "toto"
+        name = "tests_data/toto"
         self.table.save(name)
         self.assertEquals(os.path.isfile(name+'.xml'),True)
 
     def test_save_to_different_location(self,):
         fname = "titi"
-        path = 'data'
+        path = 'tests_data'
         name = os.path.join(path,fname)
         self.table.save(name=fname, path=path)
         self.assertEquals(os.path.isfile(name+'.xml'),True)
 
     def test_create_table(self,):
         self.binder.addInput(self.key)
-        self.binder.addFunction('', from_file='jscode.js')
+        self.binder.addFunction('', from_file='tests_data/jscode.js')
         self.table.addBinder(self.binder)
-        self.table.save(name='mytable')
-        self.assertEqual(os.path.isfile('mytable.xml'),True)
+        self.table.save(name='mytable', path='tests_data')
+        self.assertEqual(os.path.isfile('tests_data/mytable.xml'),True)
 
     def test_create_table_and_add_two_binders(self,):
         self.binder.addInput(self.key)
         self.binder_insert.addInput(self.key)
-        self.binder.addFunction('', from_file='jscode.js')
+        self.binder.addFunction('', from_file='tests_data/jscode.js')
         self.binder_insert.addFunction("console.log('hello this is an insert function'); ")
         self.table.addBinder(self.binder)
         self.table.addBinder(self.binder_insert)
-        self.table.save(name='mytable')
-        self.assertEqual(os.path.isfile('mytable.xml'),True)
+        self.table.save(name='mytable', path='tests_data')
+        self.assertEqual(os.path.isfile('tests_data/mytable.xml'),True)
 
     def test_create_table_with_binder(self,):
         self.binder.addInput(self.key)
-        self.binder.addFunction('', from_file='jscode.js')
+        self.binder.addFunction('', from_file='tests_data/jscode.js')
         self.table_desc['bindings'] = [self.binder]
         table = YqlTable(**self.table_desc)
-        table.save(name='mytable')
-        self.assertEqual(os.path.isfile('mytable.xml'),True)
+        table.save(name='mytable', path='tests_data')
+        self.assertEqual(os.path.isfile('tests_data/mytable.xml'),True)
 
     def tearUp(self):
-        os.path.unlink('mytest.xml')
-        os.path.unlink('toto.xml')
+        os.path.unlink('tests_data/mytest.xml')
+        os.path.unlink('tests_data/toto.xml')
         
 if '__main__' == __name__:
     unittest.main()
