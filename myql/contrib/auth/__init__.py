a587ab032b823c5ab3c3f82ee5704ce8e06bdda4
josuebrunel <josuebrunel@gmail.com>
2015-04-20 16:39:13 +0200

    removing useless stuff

--- myql/contrib/auth/__init__.py
+++ myql/contrib/auth/__init__.py
@@ -1,11 +1,1 @@
-##################################################
-#
-#   Author          : yosuke
-#   Filename        : __init__.py
-#   Description     :
-#   Creation Date   : 03-03-2015
-#   Last Modified   : Mon 20 Apr 2015 04:16:24 PM CEST
-#
-##################################################
-
 from auth import OAuth
