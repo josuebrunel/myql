23a6e246f24072339ea0f43d3dce6121e921d345
josuebrunel <josuebrunel@gmail.com>
2015-05-03 15:07:16 +0200

    logging added

--- publish_package.sh
+++ publish_package.sh
@@ -4,17 +4,24 @@
 #   Filename        : publish_package.sh
 #   Description     :
 #   Creation Date   : 03-05-2015
-#   Last Modified   : Sun May  3 14:55:43 2015
+#   Last Modified   : Sun May  3 15:07:04 2015
 #
 ##################################################
 
 if [ ! -z $1 ]; then
-    if [ $1 == 'testing' ]
-        $server=pypitest
+    if [ "$1" == "testing" ]; then
+        server=pypitest
     else
-        $server=pypi
+        server=pypi
+    fi
+else
+    server=pypitest
 fi
 
-python setup.py register -r $sever
+_debug "Publish on ${server}"
 
+_info "python setup.py register -r ${server}"
+python setup.py register -r $server
+
+_info "python setup.py sdist upload -r ${server}"
 python setup.py sdist upload -r $server
