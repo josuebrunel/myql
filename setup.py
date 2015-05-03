c4cbf8c8dede42e0a8eefa99d0a5985967824802
josuebrunel <josuebrunel@gmail.com>
2015-05-03 17:04:59 +0200

    using rst file for package description

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+import os
 from setuptools import setup, find_packages
 
 #requirements.txt
 with open('requirements.txt') as f:
   required = f.read().splitlines()
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
   name = "myql",
   version = "1.2",
   description = "Python Wrapper for the Yahoo ! Query Language",
-  long_description = "",
+  long_description = read("README.rst"),
   author = "Josue Kouka",
   author_email = "josuebrunel@gmail.com",
   url = "https://github.com/josuebrunel/MYQL",
