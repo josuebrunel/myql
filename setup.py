a9727e84f5d6d979b6f66e2e7532af85c1110c47
josuebrunel <josuebrunel@gmail.com>
2015-05-09 16:36:21 +0200

    dynamic version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 from setuptools import setup, find_packages
 
+__version__ = "1.2.1"
+
 #requirements.txt
 with open('requirements.txt') as f:
   required = f.read().splitlines()
@@ -10,13 +12,13 @@ def read(fname):
 
 setup(
   name = "myql",
-  version = "1.2.1",
+  version = __version__,
   description = "Python Wrapper for the Yahoo ! Query Language",
   long_description = read("README.rst"),
   author = "Josue Kouka",
   author_email = "josuebrunel@gmail.com",
   url = "https://github.com/josuebrunel/MYQL",
-  download_url = "https://github.com/josuebrunel/myql/archives/1.2.tar.gz",
+  download_url = "https://github.com/josuebrunel/myql/archives/{0}tar.gz".format(__version__),
   keywords = ['myql', 'yql', 'yahoo', 'query', 'language'],
   packages = find_packages(),
   classifiers = [
