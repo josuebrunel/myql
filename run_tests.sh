58e4b3a13b7c8025ebc8851419bccd91fd192023
josuebrunel <josuebrunel@gmail.com>
2015-05-12 01:17:44 +0200

    Improving test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,23 @@
-if [ ! -z $1 ]; then 
-    method=".$1"
+if [ ! -z $1 ]; then
+    suite=".$1"
+else
+    suite="all"
+fi
+
+if [ ! -z $2 ]; then 
+    method=".$2"
 else
     method=''
 fi
 
-python -m unittest tests.TestMYQL$method
-python -m unittest tests.TestTable$method
-if [ -f credentials.json ]; then
-    python -m unittest tests.TestOAuth
+if [ $suite != "all" ];then
+    python -m unittest tests$suite$method
+else
+    python -m unittest tests.TestMYQL$method
+    python -m unittest tests.TestStockParser
+    python -m unittest tests.TestTable$method
+
+    if [ -f credentials.json ]; then
+         python -m unittest tests.TestOAuth
+    fi
 fi
