cd1f8ae77797c992d6ea03f38214e85e347ee8a8
josuebrunel <josuebrunel@gmail.com>
2015-05-21 11:05:20 +0200

    Improving tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,23 @@
 set -x
 rm -rf credentials.json
-wget http://212.83.154.157/public/credentials.json
-python -m unittest tests
+wget `echo 'U2FsdGVkX19Wg/Os0JMcl3kXdAaNcgSF+fAg4oCz5zUIrCQyX3FwXeaqOAaj8YGT 
+GrYMpNIsovfk6uB+ZbHBjg==' | openssl enc -aes-128-cbc -a -d -salt -pass pass:url`
+
+if [ ! -z $1 ]; then
+    TestCase=".${1}"
+else
+    TestCase=''
+fi 
+
+if [ ! -z $2 ]; then
+    Test=".${2}"
+else
+    Test=''
+fi
+
+python -m unittest tests$TestCase$Test
+
 rm -rf credentials.json
+rm -rf yql_storage.json
+
 
