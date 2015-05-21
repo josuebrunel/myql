5befe7348ddcb7f89d7ee30202270b931ccbeb78
josuebrunel <josuebrunel@gmail.com>
2015-05-21 12:20:02 +0200

    fix #81: prettyfy OK

---
+++ myql/utils.py
@@ -0,0 +1,19 @@
+import json
+from xml.dom import minidom
+from xml.etree import cElementTree as tree
+
+def pretty_json(data):
+    data = json.loads(data)
+    return json.dumps(data, indent=4, sort_keys=True)
+
+def pretty_xml(data):
+    parsed_string = minidom.parseString(data)
+    return parsed_string.toprettyxml(indent='\t', encoding='utf-8')
+
+def prettyfy(response, format='json'):
+
+    if format=='json':
+        return pretty_json(response.content)
+    else :
+        return pretty_xml(response.content)
+
