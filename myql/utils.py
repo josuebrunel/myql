46c926a8950167852b2293195229771fef87bb50
josuebrunel <josuebrunel@gmail.com>
2015-05-22 07:58:01 +0200

    decode method added for each response.content

--- myql/utils.py
+++ myql/utils.py
@@ -3,11 +3,11 @@ from xml.dom import minidom
 from xml.etree import cElementTree as tree
 
 def pretty_json(data):
-    data = json.loads(data)
+    data = json.loads(data.decode('utf-8'))
     return json.dumps(data, indent=4, sort_keys=True)
 
 def pretty_xml(data):
-    parsed_string = minidom.parseString(data)
+    parsed_string = minidom.parseString(data.decode('utf-8'))
     return parsed_string.toprettyxml(indent='\t', encoding='utf-8')
 
 def prettyfy(response, format='json'):
