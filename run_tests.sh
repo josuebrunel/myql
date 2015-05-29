4c0cd4833ea7dd1f965a32cefb214a4bea746fa7
josuebrunel <josuebrunel@gmail.com>
2015-05-29 11:23:16 +0200

    run_test changed

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
-rm -rf credentials.json
-wget `echo 'U2FsdGVkX19Wg/Os0JMcl3kXdAaNcgSF+fAg4oCz5zUIrCQyX3FwXeaqOAaj8YGT 
-GrYMpNIsovfk6uB+ZbHBjg==' | openssl enc -aes-128-cbc -a -d -salt -pass pass:url`
+#rm -rf credentials.json
+#wget `echo 'U2FsdGVkX19Wg/Os0JMcl3kXdAaNcgSF+fAg4oCz5zUIrCQyX3FwXeaqOAaj8YGT 
+#GrYMpNIsovfk6uB+ZbHBjg==' | openssl enc -aes-128-cbc -a -d -salt -pass pass:url`
 
 if [ ! -z $1 ]; then
     TestCase=".${1}"
