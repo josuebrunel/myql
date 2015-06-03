58126b2e4cea72ccab89c0cb81efd2b5d1934b7f
josuebrunel <josuebrunel@gmail.com>
2015-06-03 09:23:09 +0200

    fix #105: raw_input not defined in Python3

--- myql/contrib/auth/yoauth.py
+++ myql/contrib/auth/yoauth.py
@@ -1,6 +1,12 @@
 """
 YOAuth is inspired from Darren Kempiners YahooAPI https://github.com/dkempiners/python-yahooapi/blob/master/yahooapi.py
 """
+try:
+    input = raw_input
+except (NameError,):
+    pass
+
+
 from __future__ import absolute_import
 
 import json
@@ -66,7 +72,7 @@ class YOAuth(object):
             authorize_url = AUTHORIZE_TOKEN_URL+request_token
             logging.debug(authorize_url)
             webbrowser.open(authorize_url)
-            verifier = raw_input("Enter verifier : ")
+            verifier = input("Enter verifier : ")
             logging.debug("VERIFIER = {0}".format(verifier))
 
             self.token_time = time.time()
