a2717daa93ff4eec1362f7d4acb581b52a87d00c
josuebrunel <josuebrunel@gmail.com>
2015-06-06 02:32:20 +0200

    securing credentials

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,4 @@
-set +x
-rm -rf credentials.json
-wget `echo 'U2FsdGVkX19Wg/Os0JMcl3kXdAaNcgSF+fAg4oCz5zUIrCQyX3FwXeaqOAaj8YGT 
-GrYMpNIsovfk6uB+ZbHBjg==' | openssl enc -aes-128-cbc -a -d -salt -pass pass:url`
-
-set -x
+wget $credentials
 
 if [ ! -z $1 ]; then
     TestCase=".${1}"
