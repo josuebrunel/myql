c54c5d1768b091da2ec79a83fcd5fbe441430de3
josuebrunel <josuebrunel@gmail.com>
2015-06-09 05:09:36 +0200

    cleaning the error house

--- myql/errors.py
+++ myql/errors.py
@@ -8,30 +8,4 @@ class NoTableSelectedError(Exception):
 
   def __str__(self):
     return repr(self.msg)
-
-class NoConfigFileError(Exception):
-  '''Error raised when the config file passed
-  doesn't exist 
-  '''
-  def __init__(self, msg=None):
-    if not msg:
-      msg = 'Config file is invalid or doesn\'t exist'
-
-    self.msg = msg
-
-  def __str__(self,):
-    return repr(self.msg)
-
-
-class NoConfigParameter(Exception):
-  '''Error raised when config parameters don't exist
-  '''
-  def __init__(self, msg=None):
-    if not msg:
-      msg = 'Invalid config parameters'
-
-    self.msg = msg
-
-  def __str__(self,):
-    return repr(self.msg)
-  
+ 
