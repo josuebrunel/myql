622297b32391a49ff858ae224e5bcb3e939d0bb5
josuebrunel <josuebrunel@gmail.com>
2015-06-16 07:57:00 +0200

    #126 : improving library health

--- myql/contrib/finance/stockscraper/stockretriever.py
+++ myql/contrib/finance/stockscraper/stockretriever.py
@@ -21,21 +21,21 @@ class StockRetriever(YQL):
         super(StockRetriever, self).__init__(community=True, format=format, debug=debug, oauth=oauth)
 
 
-    def get_current_info(self, symbolList, columns=None, format='json'):
+    def get_current_info(self, symbolList, columns=None):
         """get_current_info() uses the yahoo.finance.quotes datatable to get all of the stock information presented in the main table on a typical stock page 
         and a bunch of data from the key statistics page.
         """
         response = self.select('yahoo.finance.quotes',columns).where(['symbol','in',symbolList])
         return response
 
-    def get_news_feed(self, symbol,format='json'):
+    def get_news_feed(self, symbol):
         """get_news_feed() uses the rss data table to get rss feeds under the Headlines and Financial Blogs headings on a typical stock page.
         """
         rss_url='http://finance.yahoo.com/rss/headline?s={0}'.format(symbol)
         response = self.select('rss',['title','link','description'],limit=2).where(['url','=',rss_url])
         return response
 
-    def get_historical_info(self, symbol,items=None, startDate=None, endDate=None, limit=None, format='json'):
+    def get_historical_info(self, symbol,items=None, startDate=None, endDate=None, limit=None):
         """get_historical_info() uses the csv datatable to retrieve all available historical data on a typical historical prices page
         """
         today = date.today()
@@ -48,19 +48,19 @@ class StockRetriever(YQL):
         response = self.select('yahoo.finance.historicaldata',items,limit).where(['symbol','=',symbol],['startDate','=',startDate],['endDate','=',endDate])
         return response
 
-    def get_options_info(self, symbol, items=None, expiration='', format=format):
+    def get_options_info(self, symbol, items=None, expiration=''):
         """get_options_data() uses the yahoo.finance.options table to retrieve call and put options from the options page.
         """
         response = self.select('yahoo.finance.options',items).where(['symbol','=',symbol],[] if not expiration else ['expiration','=',expiration])
         return response
 
-    def get_index_summary(self, symbol, items=None,format='json'):
+    def get_index_summary(self, symbol, items=None):
         """
         """
         response = self.select('yahoo.finance.quoteslist',items).where(['symbol','=',symbol])
         return response
 
-    def get_industry_index(self, index_id,items=None,format='json'):
+    def get_industry_index(self, index_id,items=None):
         """retrieves all symbols that belong to an industry.
         """
         response = self.select('yahoo.finance.industry',items).where(['id','=',index_id])
