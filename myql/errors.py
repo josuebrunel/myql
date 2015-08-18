378b1e77976a758fabe56fdff426b665b5dad28a
Josue Kouka <josuebrunel@gmail.com>
2015-08-18 15:30:48 +0200

    cleaning the exception house

--- myql/errors.py
+++ myql/errors.py
@@ -2,8 +2,6 @@ class NoTableSelectedError(Exception):
     '''Error raised when no table has been selected
     '''
     def __init__(self, msg=None):
-        if not msg:
-            msg = 'No table selected'
         self.msg = msg
 
     def __str__(self):
